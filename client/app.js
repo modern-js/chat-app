c0ff4c733ebf92bf690217d5a0cfc9bf5c13f8a5
Jimsher Skhirtladze <jskhirtladze@gmail.com>
2017-10-19 14:41:27 +0300

    add front-end login/logout

--- client/app.js
+++ client/app.js
@@ -1,3 +1,4 @@
+
 const makeButton = (text, action) => {
   const btn = document.createElement('button');
   btn.textContent = text;
@@ -6,6 +7,7 @@ const makeButton = (text, action) => {
 };
 
 const root = document.getElementById('app');
+
 const output = document.createElement('div');
 
 const withLogResponse = promise => (
@@ -28,3 +30,41 @@ const withLogResponse = promise => (
 ].forEach(button => root.appendChild(button));
 
 root.appendChild(output);
+
+
+// everything above this eventually needs to be deleted
+
+const mainLoop = function() {
+  window.api.me().then(x=>x.text()).then(x=>{
+    x = JSON.parse(x);
+    if (x.status=='OK') {
+      showApp();
+    }
+    else {
+      showForm();
+    }
+    setTimeout(mainLoop, 1000);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", mainLoop);
+
+document.getElementById('login').onclick = function(){
+  let username = document.getElementById('user_text').value;
+  let password = document.getElementById('pass_text').value;
+  window.api.login({username:username,password:password});
+}
+
+document.getElementById('logout').onclick = function(){
+  window.api.logout();
+}
+
+const showForm = function() {
+  document.getElementById('app').style.visibility='hidden';
+  document.getElementById('login_form').style.visibility='visible';
+}
+
+const showApp = function() {
+  document.getElementById('app').style.visibility='visible';
+  document.getElementById('login_form').style.visibility='hidden';
+}
